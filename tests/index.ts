7edc2758722316d0192fe4d83a42317710cc0330
Ivan Kozik <ivan@ludios.org>
2023-04-01 13:06:32 +0000

    tests: use t.throws to ensure that the A.* asserts actually throw

--- tests/index.ts
+++ tests/index.ts
@@ -9,88 +9,45 @@ test('A', t => {
 	t.throws(() => A(false), {instanceOf: AssertionError, message: /^A\(\.\.\.\): false not truthy$/});
 	t.throws(() => A(0),     {instanceOf: AssertionError, message: /^A\(\.\.\.\): 0 not truthy$/});
 	t.throws(() => A(""),    {instanceOf: AssertionError, message: /^A\(\.\.\.\): '' not truthy$/});
-	try {
-		A(0);
-	} catch (e) {
-		t.is(e.toString(), "AssertionError: A(...): 0 not truthy");
-	}
-	try {
-		A(0, "explanation");
-	} catch (e) {
-		t.is(e.toString(), "AssertionError: A(...): 0 not truthy: explanation");
-	}
-	try {
-		A(0, A.fn, () => "explanation");
-	} catch (e) {
-		t.is(e.toString(), "AssertionError: A(...): 0 not truthy: explanation");
-	}
-	try {
-		// @ts-expect-error
-		A(0, A.fn, () => undefined);
-	} catch (e) {
-		t.is(e.toString(), "AssertionError: A(...): 0 not truthy: undefined");
-	}
-	try {
-		// number messages are supported despite not being validly typed
-		// @ts-expect-error
-		A(0, 0);
-	} catch (e) {
-		t.is(e.toString(), "AssertionError: A(...): 0 not truthy: 0");
-	}
+	t.throws(() => A(0), {instanceOf: AssertionError, message: "A(...): 0 not truthy"});
+	t.throws(() => A(0, "explanation"), {instanceOf: AssertionError, message: "A(...): 0 not truthy: explanation"});
+	t.throws(() => A(0, A.fn, () => "explanation"), {instanceOf: AssertionError, message: "A(...): 0 not truthy: explanation"});
+	// undefined return is supported despite not being validly typed
+	// @ts-expect-error
+	t.throws(() => A(0, A.fn, () => undefined), {instanceOf: AssertionError, message: "A(...): 0 not truthy: undefined"});
+	// number messages are supported despite not being validly typed
+	// @ts-expect-error
+	t.throws(() => A(0, 0), {instanceOf: AssertionError, message: "A(...): 0 not truthy: 0"});
 });
 
 test('A.eq', t => {
 	A.eq(3, 3);
-	try {
-		A.eq(3, "3");
-	} catch (e) {
-		t.is(e.toString(), "AssertionError: A.eq(...): 3 !== '3'");
-	}
+	t.throws(() => A.eq(3, "3"), {instanceOf: AssertionError, message: "A.eq(...): 3 !== '3'"});
 });
 
 test('A.neq', t => {
 	A.neq(3, "3");
-	try {
-		A.neq(3, 3);
-	} catch (e) {
-		t.is(e.toString(), "AssertionError: A.neq(...): 3 === 3");
-	}
+	t.throws(() => A.neq(3, 3), {instanceOf: AssertionError, message: "A.neq(...): 3 === 3"});
 });
 
 test('A.lt', t => {
 	A.lt(3, 4);
-	try {
-		A.lt(4, 3);
-	} catch (e) {
-		t.is(e.toString(), "AssertionError: A.lt(...): !(4 < 3)");
-	}
+	t.throws(() => A.lt(4, 3), {instanceOf: AssertionError, message: "A.lt(...): !(4 < 3)"});
 });
 
 test('A.lte', t => {
 	A.lte(3, 3);
 	A.lte(2, 3);
-	try {
-		A.lte(4, 3);
-	} catch (e) {
-		t.is(e.toString(), "AssertionError: A.lte(...): !(4 <= 3)");
-	}
+	t.throws(() => A.lte(4, 3), {instanceOf: AssertionError, message: "A.lte(...): !(4 <= 3)"});
 });
 
 test('A.gt', t => {
 	A.gt(4, 3);
-	try {
-		A.gt(3, 4);
-	} catch (e) {
-		t.is(e.toString(), "AssertionError: A.gt(...): !(3 > 4)");
-	}
+	t.throws(() => A.gt(3, 4), {instanceOf: AssertionError, message: "A.gt(...): !(3 > 4)"});
 });
 
 test('A.gte', t => {
 	A.gte(3, 3);
 	A.gte(3, 2);
-	try {
-		A.gte(3, 4);
-	} catch (e) {
-		t.is(e.toString(), "AssertionError: A.gte(...): !(3 >= 4)");
-	}
+	t.throws(() => A.gte(3, 4), {instanceOf: AssertionError, message: "A.gte(...): !(3 >= 4)"});
 });
